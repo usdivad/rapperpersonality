48f4ca7330defee6ee20faf3c18297f0777f0dcd
David Su <david.d.su@gmail.com>
2014-01-22 15:55:04 -0500

    first commit; implemented basic manhattan distance fn with 3 params

---
+++ params.js
@@ -0,0 +1,48 @@
+//out of 10
+
+// weed < pills < cocaine < acid
+var drugList = {
+	"Acid": 10,
+	"Cocaine": 5,
+	"Crack, Cocaine": 5,
+	"Crack, Pills": 7,
+	"Lean, Weed": 6,
+	"Molly, Weed": 6,
+	"None": 0,
+	"Pills": 4,
+	"Pills, Weed": 6,
+	"Weed": 3,
+	"Weed, Acid": 9,
+	"Weed, Cocaine": 8,
+	"Weed, Cocaine, Crack": 8,
+	"Weed, Lean": 6,
+	"Weed, Lean, Pills": 7,
+	"Weed, Molly": 6
+}
+
+//5 cats: mt dew < beer < bottomshelf < champagne/wine < topshelf
+var drinkList = {
+	"Beer": 2.5,
+	"beer/bottom shelf liquor": 4.5,
+	"bottom shelf liquor": 5,
+	"Champagne/Wine": 7.5,
+	"Mountain Dew": 0,
+	"top shelf liquor": 10
+}
+
+//vinyl < cassettes < cds < mixtapes < mp3s
+var ageList = {
+	"Cassettes": 2.5,
+	"CDs": 5,
+	"Mixtapes": 7.5,
+	"MP3": 10,
+	"Vinyl": 0
+}
+
+
+
+var allParams = {
+	"Drug of choice": drugList,
+	"Drink of choice": drinkList,
+	"Age/Audio Format": ageList
+};
---
+++ parse.js
@@ -0,0 +1,126 @@
+
+//Get rapper score (overall)
+function get_score(rapper) {
+	var r_score = 0;
+
+	//get attrs
+	//var name = rapper["Rapper"];
+	var drug = rapper["Drug of choice"];
+	var drink = rapper["Drink of choice"];
+	var age = rapper["Age/Audio Format"];
+	var fashion = rapper["Fashion"];
+	var region = rapper["Region"];
+	var criminal = rapper["Criminal History"];
+	var fitness = rapper["Food/Fitness/Body Type"];
+	var intelligence = rapper["Intelligence"];
+	var pimp = rapper["Pimp Hand"];
+	var tattoos = rapper["Tattoos"];
+	var sound = rapper["Sound"];
+
+
+	//making the score
+	//r_score = drugList[drug] + drinkList[drink] + ageList[age];
+	r_score = parse(drugList, drug) + parse(drinkList, drink) + parse(ageList, age);
+	return r_score;
+}
+
+function convert_to_number(rapper) {
+	//get attrs
+	var drug = rapper["Drug of choice"];
+	var drink = rapper["Drink of choice"];
+	var age = rapper["Age/Audio Format"];
+}
+
+//Parse an individual attribute from a list of scores
+function parse(list, attr) {
+	if (typeof list[attr] != "undefined") {
+		//console.log()
+		return list[attr];
+	}
+	else {
+		console.log("No " + attr + " found in list!");
+		return 5;
+	}
+}
+
+//Calculate Manhattan distance using all available params
+function manhattan_distance(rapper1, rapper2, params) {
+	var md = 0;
+	var i = 0;
+	for (key in rapper1) {
+		//get the drug they like
+		var param_rapper1 = rapper1[key];
+		var param_rapper2 = rapper2[key];
+		//get the list with the right params
+		var list = params[key];
+		if (typeof list != "undefined") {
+			var score_rapper1 = list[param_rapper1];
+			var score_rapper2 = list[param_rapper2];
+			//console.log(score_rapper1);
+			if (typeof score_rapper1 != "undefined" && typeof score_rapper2 != "undefined") {
+				//console.log(drug_rapper1, score_rapper1);
+				md += attribute_difference(score_rapper1, score_rapper2);
+			}
+		}
+		
+		i++;
+	}
+	return md;
+}
+
+//Diff between attributes
+function attribute_difference(attr1, attr2) {
+	return Math.abs(attr2 - attr1);
+}
+
+function find_closest(rapper1, rapperList, paramList) {
+	for (var i=0; i<rapperList.length; i++) {
+		var rapper2 = rapperList[i];
+		var dist = manhattan_distance(rapper1, rapper2, paramList);
+		if (typeof dist != "undefined") {
+			rapper2["Distance"] = dist;
+		}
+	}
+	rapperList.sort(function(a,b) {
+		var aDist = a["Distance"];
+		var bDist = b["Distance"];
+		if (aDist < bDist) {
+			return -1;
+		}
+		else if (aDist > bDist) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	})
+	return rapperList;
+}
+
+//Maindo
+function main() {
+	var rappers;
+
+	//JSON req
+	$.getJSON("rapper_stats.json", function(data) {
+		console.log(data);
+		rappers = data;
+		console.log(rappers.length);
+
+		//for (rapper in rappers) {
+		for (var i=0; i<rappers.length; i++) {
+			var rapper = rappers[i];
+			//console.log(rapper);
+			rapper_score = get_score(rapper);
+			//console.log(rapper["Rapper"] + ": " + rapper_score);
+		}
+
+		var r0 = rappers[0];
+		var r1 = rappers[23];
+		//var r1 = {"Rapper":"Mister Twister","Drug of choice":"Weed","Drink of choice":"Beer","Age/Audio Format":"MP3","Fashion":"Hipster","Region":"West Coast","Criminal History":"Might have killed someone","Food/Fitness/Body Type":"stocky","Intelligence":"Dumb","Pimp Hand":"Legit Pimp","Tattoos":"many","Sound":"Classic"};
+		console.log("dist between " + r0["Rapper"] + " and " + r1["Rapper"] + " is " + manhattan_distance(r0, r1, allParams));
+		console.log(find_closest(r1, rappers, allParams));
+	});
+}
+
+main();
