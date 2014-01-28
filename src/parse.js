1e52029b907d0bfa4acce7c61901be1f616106a2
David Su <david.d.su@gmail.com>
2014-01-28 18:34:47 -0500

    clean parse.js, add parse_tester.js as sep

--- src/parse.js
+++ src/parse.js
@@ -2,18 +2,18 @@
 //uses matches.js and manhattan
 //1/28 removed: paramList parameter
 function calculatePersonality(user, data) {
-
 		var NUM_OUTPUT = 10;
 		var str = "<br><br>";
 
-		//Sample output; ordered by score function. Alternate suggestions are shuffled
+		//Sample output; region-filtered then ordered by score function. Only alternate suggestions (non-first) are shuffled
 		var data_filtered = filter_by_region(user, data);
 		var most_matches = find_most_matches(user, data_filtered);
 		var most_matches_shuffled = find_most_matches(user, shuffle(most_matches));
 		var who = most_matches[0];
+		//First
 		str += "You are <strong>" + who["Rapper"] + "</strong>! You have "
 				+ "a compatibility score of " + who["Matches"] + "<br><br>";
-			//+ who["Matches"] + " traits in common and a rap proximity of " + who["Distance"] + "<br><br>";
+		//Rest
 		str += "However, you could also be:<br>"
 		for (var i=1; i<NUM_OUTPUT; i++) {
 			var alt_who = most_matches_shuffled[i];
@@ -23,13 +23,13 @@ function calculatePersonality(user, data) {
 			else {
 				str += "<strong>" + alt_who["Rapper"] + "</strong> (compatibility score of "
 					+ alt_who["Matches"] + ") <br>";
-					//+ alt_who["Matches"] + " traits and a rap proximity of " + alt_who["Distance"] + ") <br>";
 			}
 		}
 
 		return str;
 }
 
+//Filters out rappers who aren't in user's chosen region (RIVALRIES!)
 function filter_by_region(user, data) {
 	console.log("filtering");
 	var filtered = [];
@@ -74,53 +74,4 @@ function shuffle(original_array) {
   }
 
   return array;
-}
-
-//Tester
-//1/28: removed paramList parameter
-function test_parse(data) {
-
-		//Data parsing
-		console.log("From test_parse: ");
-		console.log(data);
-		rappers = data;
-		console.log(rappers.length);
-
-		//Tester params
-		var str = "";
-		var NUM_OUTPUT = 10;
-
-		//case 1
-		var r1 = {"Rapper":"Mister Twister","Decade":"1990s, 2000s, 2010s","Region":"East Coast","Fashion":"Hipster","Tattoos":"Facial","Food_Fitness_BodyType":"short","Intelligence":"Dumb","CriminalHistory":"None","PimpHand":"Legit Pimp","Sound":"Classic","DrinkOfChoice":"Beer","DrugOfChoice":"Weed"};
-		//r1 = {"Rapper":"Nelly","Decade":"1990s, 2000s, 2010s","Region":"Mid West","Fashion":"T shirt & Jeans","Tattoos":"a few","Food_Fitness_BodyType":"athletic","Intelligence":"Dumb","PimpHand":"Semi-Pimp","CriminalHistory":"None","Sound":"Pop","DrinkOfChoice":"Champagne/Wine","DrugOfChoice":"Weed"};
-
-		//case 2
-		//r1 = {"Rapper":"Slim Jim","Drug of choice":"Acid","Drink of choice":"Champagne/Wine","Age/Audio Format":"Cassettes","Fashion":"Upscale","Region":"Dirty South","Criminal History":"Drug dealer","Food/Fitness/Body Type":"tall, fat","Intelligence":"Smart","Pimp Hand":"Pussy whipped","Tattoos":"Facial","Sound":"Pop/underground/alternative"};
-
-		//case 3
-		for (key in rappers[19]) {
-			r1[key] = rappers[19][key];
-		}
-		//r1["Sound"] = "Pop";
-		//console.log(r1);
-
-		//Testing Manhattan distance
-		/*
-		var closest = find_closest(r1, rappers, paramList);
-		str += "Closest rappers according to Manhattan distance are: \n";
-		for (var i=0; i<NUM_OUTPUT; i++) {
-			str += closest[i]["Rapper"] + " with a distance of " + closest[i]["Distance"] + "\n";
-		}
-		//console.log(closest);
-		*/
-
-		//Testing matches
-		var most_matches = find_most_matches(r1, rappers);
-		str += "Your name is " + r1["Rapper"] + "! \n";
-		str += "\n Closest rappers according to most matches are: \n";
-		for (var i=0; i<NUM_OUTPUT; i++) {
-			str += most_matches[i]["Rapper"] + " with " + most_matches[i]["Matches"] + " matches \n";
-		}
-		//console.log(most_matches);
-		console.log(str);
 }
---
+++ src/parse_tester.js
@@ -0,0 +1,48 @@
+//Tester
+//1/28: removed paramList parameter
+function test_parse(data) {
+
+		//Data parsing
+		console.log("From test_parse: ");
+		console.log(data);
+		rappers = data;
+		console.log(rappers.length);
+
+		//Tester params
+		var str = "";
+		var NUM_OUTPUT = 10;
+
+		//case 1
+		var r1 = {"Rapper":"Mister Twister","Decade":"1990s, 2000s, 2010s","Region":"East Coast","Fashion":"Hipster","Tattoos":"Facial","Food_Fitness_BodyType":"short","Intelligence":"Dumb","CriminalHistory":"None","PimpHand":"Legit Pimp","Sound":"Classic","DrinkOfChoice":"Beer","DrugOfChoice":"Weed"};
+		//r1 = {"Rapper":"Nelly","Decade":"1990s, 2000s, 2010s","Region":"Mid West","Fashion":"T shirt & Jeans","Tattoos":"a few","Food_Fitness_BodyType":"athletic","Intelligence":"Dumb","PimpHand":"Semi-Pimp","CriminalHistory":"None","Sound":"Pop","DrinkOfChoice":"Champagne/Wine","DrugOfChoice":"Weed"};
+
+		//case 2
+		//r1 = {"Rapper":"Slim Jim","Drug of choice":"Acid","Drink of choice":"Champagne/Wine","Age/Audio Format":"Cassettes","Fashion":"Upscale","Region":"Dirty South","Criminal History":"Drug dealer","Food/Fitness/Body Type":"tall, fat","Intelligence":"Smart","Pimp Hand":"Pussy whipped","Tattoos":"Facial","Sound":"Pop/underground/alternative"};
+
+		//case 3
+		for (key in rappers[19]) {
+			r1[key] = rappers[19][key];
+		}
+		//r1["Sound"] = "Pop";
+		//console.log(r1);
+
+		//Testing Manhattan distance
+		/*
+		var closest = find_closest(r1, rappers, paramList);
+		str += "Closest rappers according to Manhattan distance are: \n";
+		for (var i=0; i<NUM_OUTPUT; i++) {
+			str += closest[i]["Rapper"] + " with a distance of " + closest[i]["Distance"] + "\n";
+		}
+		//console.log(closest);
+		*/
+
+		//Testing matches
+		var most_matches = find_most_matches(r1, rappers);
+		str += "Your name is " + r1["Rapper"] + "! \n";
+		str += "\n Closest rappers according to most matches are: \n";
+		for (var i=0; i<NUM_OUTPUT; i++) {
+			str += most_matches[i]["Rapper"] + " with " + most_matches[i]["Matches"] + " matches \n";
+		}
+		//console.log(most_matches);
+		console.log(str);
+}
