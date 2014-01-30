6bb50973e29fc62ef5dd43c73058589c6bc53428
David Su <david.d.su@gmail.com>
2014-01-30 15:15:44 -0500

    update merge; puts it all in the theme PHP file now

--- merge.sh
+++ merge.sh
@@ -3,5 +3,12 @@
 cd src
 cat matches.js parse.js parse_tester.js input.js ../main.js > all.js
 
+cd ..
+cat page-rapper-personality-quiz_template.php > page-rapper-personality-quiz.php
+echo '<script>' >> page-rapper-personality-quiz.php
+echo '//Merged' `date +%Y`'/'`date +%m`'/'`date +%d`' '`date +%H`':'`date +%M` >> page-rapper-personality-quiz.php 
+cat src/all.js >> page-rapper-personality-quiz.php
+echo '</script>' >> page-rapper-personality-quiz.php
+
 #All .js files with date
 #cat *.js ../main.js > all_`date +%Y%m%d%H%M`.js
