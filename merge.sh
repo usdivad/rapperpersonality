cd16df5bdc2f32b045b7cefa1320a001e2964735
David Su <david.d.su@gmail.com>
2014-02-03 18:06:39 -0500

    update merge header

--- merge.sh
+++ merge.sh
@@ -4,9 +4,10 @@ cd src
 cat matches.js parse.js parse_tester.js input.js ../main.js > all.js
 
 cd ..
-cat page-rapper-personality-quiz_template.php > page-rapper-personality-quiz.php
+echo '<!--Updated' `date +%Y`'/'`date +%m`'/'`date +%d`' '`date +%H`':'`date +%M` 'by David Su-->' > page-rapper-personality-quiz.php
+cat page-rapper-personality-quiz_template.php >> page-rapper-personality-quiz.php
 echo '<script>' >> page-rapper-personality-quiz.php
-echo '//Merged' `date +%Y`'/'`date +%m`'/'`date +%d`' '`date +%H`':'`date +%M` >> page-rapper-personality-quiz.php 
+echo '//Merged' `date +%Y`'/'`date +%m`'/'`date +%d`' '`date +%H`':'`date +%M` 'by David Su http://usdivad.com/' >> page-rapper-personality-quiz.php 
 cat src/all.js >> page-rapper-personality-quiz.php
 echo '</script>' >> page-rapper-personality-quiz.php
 
