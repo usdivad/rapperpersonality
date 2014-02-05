8ec669a45eb2fbdff7834f8661a2a7a054cdda92
David Su <david.d.su@gmail.com>
2014-02-05 14:17:19 -0500

    move fb script async

--- page-rapper-personality-quiz_template.php
+++ page-rapper-personality-quiz_template.php
@@ -1,5 +1,37 @@
 <?php get_header(); ?>
 <div id="fb-root"></div>
+<script>
+//fb
+  window.fbAsyncInit = function() {
+          FB.init({
+            appId      : 1375271719406903,
+            status     : true,
+            xfbml      : true,
+            oauth      : true
+          });
+        };
+
+        /*(function(d, s, id){
+           var js, fjs = d.getElementsByTagName(s)[0];
+           if (d.getElementById(id)) {return;}
+           js = d.createElement(s); js.id = id;
+           js.src = "//connect.facebook.net/en_US/all.js";
+           fjs.parentNode.insertBefore(js, fjs);
+         }(document, 'script', 'facebook-jssdk'));*/
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1375271719406903";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+
+      
+//twitter
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+</script>
 <div id="main-content">
   <div id="left-column">
     <article class="post">
@@ -37,36 +69,10 @@
   input:checked {
     background-color: red;
   }
+
+  .post a {
+    font-size: 100%;
+  }
 </style>
 
-<script>
-//fb
-  window.fbAsyncInit = function() {
-          FB.init({
-            appId      : 1375271719406903,
-            status     : true,
-            xfbml      : true
-          });
-        };
 
-        /*(function(d, s, id){
-           var js, fjs = d.getElementsByTagName(s)[0];
-           if (d.getElementById(id)) {return;}
-           js = d.createElement(s); js.id = id;
-           js.src = "//connect.facebook.net/en_US/all.js";
-           fjs.parentNode.insertBefore(js, fjs);
-         }(document, 'script', 'facebook-jssdk'));*/
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=1375271719406903";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-
-      
-//twitter
-  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-
-</script>
